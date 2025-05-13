c926cfb5984807a649298ed452aa4ec99b619d74
Le Toan <33773069+toanld@users.noreply.github.com>
2025-05-13 15:02:25 +0700

    Update ResponseData.php

--- src/DTO/ResponseData.php
+++ src/DTO/ResponseData.php
@@ -23,63 +23,23 @@ final class ResponseData extends DataTransferObject
      * @inheritDoc
      */
     public function __construct(
-        /**
-         * ID of the request which later can be used for cost request
-         *
-         * @var string
-         */
         public string $id,
 
-        /**
-         * Model provider e.g. HuggingFace
-         *
-         * @var string|null
-         */
-        public ?string $provider = null,
-
-        /**
-         * Name of the model e.g. mistralai/mistral-7b-instruct:free
-         *
-         * @var string
-         */
         public string $model,
 
-        /**
-         * e.g. 'chat.completion' | 'chat.completion.chunk'
-         *
-         * @var string
-         */
         public string $object,
 
-        /**
-         * Unix timestamp of created_at e.g. 1715621307
-         *
-         * @var int
-         */
         public int $created,
 
-        /**
-         * Depending on whether you set "stream" to "true"
-         * and whether you passed in "messages" or a "prompt", you get a different output shape.
-         *
-         * @var StreamingChoiceData[]|NonStreamingChoiceData[]|NonChatChoiceData[]|null
-         */
+        public ?string $provider = null,
+
         public ?array $choices = null,
 
-        /**
-         * Usage information of api request.
-         *
-         * @var UsageData|null
-         */
         public ?UsageData $usage = null,
 
-        /**
-         * If using Perplexity Sonar, will return citations
-         *
-         * @var string[]|null
-         */
         public ?array $citations = null,
     ) {
         parent::__construct(...func_get_args());
     }
+
 }
